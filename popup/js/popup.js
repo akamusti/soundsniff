04ecbcddfd5e1eec99773940af79b05357438f13
akamusti <insanmusti7@proton.me>
2026-09-10 14:22:25 +0300

    Add small by akamusti credit to settings (v1.4.3)

--- popup/js/popup.js
+++ popup/js/popup.js
@@ -42,7 +42,7 @@
   var settingsSaved = $('settings-saved');
   var languageSelect = $('language-select');
   var aboutText = $('about-text');
-  var APP_VERSION = '1.4.2';
+  var APP_VERSION = '1.4.3';
 
   function t(key, vars) {
     try {
